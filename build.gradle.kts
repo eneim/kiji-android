fce90c7329e4c0ef14947362dedce906c86d2735
Nam Nguyen <1776230+eneim@users.noreply.github.com>
2022-09-04 21:36:18 +0900

    Convert settings.gradle and top level build.gradle to kts files.

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022. Nam Nguyen, nam@ene.im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+buildscript {
+    val compose_compiler_version: String by extra("1.3.0")
+    val compose_version: String by extra("1.3.0-beta01")
+}
+
+plugins {
+    id("com.android.application") version "7.3.0-rc01" apply false
+    id("com.android.library") version "7.3.0-rc01" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+allprojects {
+    afterEvaluate {
+        if (hasProperty("android") && hasProperty("dependencies")) {
+            dependencies {
+                "coreLibraryDesugaring"("com.android.tools:desugar_jdk_libs:1.2.2")
+            }
+        }
+    }
+
+    plugins.withType<com.android.build.gradle.BasePlugin>().configureEach {
+        extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+            compileSdkVersion(33)
+            defaultConfig {
+                minSdk = 21
+                targetSdk = 33
+            }
+
+            compileOptions {
+                isCoreLibraryDesugaringEnabled = true
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = freeCompilerArgs + arrayOf("-opt-in=kotlin.RequiresOptIn")
+        }
+    }
+}
